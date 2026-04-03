80ad3d3b10e9e50b1f9c2d58b1d6777679b04cf9
kiteofyte <keruells@gmail.com>
2026-04-03 13:17:38 +0300

    feachure(validation): созданы pydantic-схемы для добавления задачи и категории

---
+++ backend/app/schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from datetime import date
+from typing import Optional
+
+class CategoryCreate(BaseModel):
+    name: str
+
+class TaskCreate(BaseModel):
+    date_begin: date
+    date_fact_end: date | None
+    date_plan_end: date | None
+    category_id: int
+    author: str
+    name: str
+    description: str
+    link: str | None
+    channel: str
+    id_parent_task: int | None
