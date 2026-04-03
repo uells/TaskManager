5aadb459f4c9e61473d48d88f488c2d188ce97e6
kiteofyte <keruells@gmail.com>
2026-04-03 15:14:26 +0300

    feachure(database): Созданы функции удаления и создания таблиц БД

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
 
-DB_URL = "sqllite + aiosqlite:///./data/tasks.db"
+DB_URL = "sqlite + aiosqlite:///./data/tasks.db"
 
 class Base(DeclarativeBase):
     pass
@@ -13,3 +13,11 @@ async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 async def get_async_session():
     async with async_session_maker() as session:
         yield session
+
+async def create_database():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+async def clear_database():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
