7a06bf797323fd7108ba13e8d5b439e4d281a401
kiteofyte <keruells@gmail.com>
2026-04-06 20:38:32 +0300

    feature(main): включен роутер

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, HTTPException
 from database import create_database
+from routers import router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -8,3 +9,5 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+app.include_router(router, prefix="/api/v1")
