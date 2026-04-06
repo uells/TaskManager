fc7d639b8b8a2c8587bea9c13d7c3bb1da118297
kiteofyte <keruells@gmail.com>
2026-04-06 20:40:09 +0300

    feature(routers): Добавлены эндпоинты добавления задач и категорий

--- backend/app/routers.py
+++ backend/app/routers.py
@@ -1,5 +1,17 @@
-from fastapi import APIRouter, HTTPException
-
-from schemas import TaskCreate
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from database import get_async_session
+from schemas import TaskCreate, CategoryCreate
+from repositories import TaskRepository, CategoryRepository
 
 router = APIRouter()
+
+@router.post("/task")
+async def create_task(task: TaskCreate, session: AsyncSession = Depends(get_async_session)):
+    repo = TaskRepository(session)
+    return await repo.create(task)
+
+@router.post("/category")
+async def create_category(category: CategoryCreate, session: AsyncSession = Depends(get_async_session)):
+    repo = CategoryRepository(session)
+    return await repo.create(category)
