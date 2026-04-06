8d782e6049865ede25a75b7bd0a90c9312abf001
kiteofyte <keruells@gmail.com>
2026-04-06 20:31:04 +0300

    fix(database): Исправлен путь к БД

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
 
-DB_URL = "sqlite + aiosqlite:///./data/tasks.db"
+DB_URL = "sqlite+aiosqlite:///./tasks.db"
 
 class Base(DeclarativeBase):
     pass
