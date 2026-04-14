ef4a42a8dc3e0e0da10e1a9162903fc0ddf72231
kiteofyte <keruells@gmail.com>
2026-04-14 15:34:01 +0300

    refactor(env): Добавлена переменная окружения

--- backend/app/config.py
+++ backend/app/config.py
@@ -7,6 +7,7 @@ DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
