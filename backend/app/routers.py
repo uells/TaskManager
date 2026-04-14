48717ec119e7c2f4483a1756eb0b6ad02f5b8783
kiteofyte <keruells@gmail.com>
2026-04-15 00:43:03 +0300

    feature(auth): защищены endpoints задач и категорий через UserDep, рефакторинг на SessionDep

--- backend/app/routers.py
+++ backend/app/routers.py
@@ -1,14 +1,17 @@
 from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.ext.asyncio import AsyncSession
-from database import get_async_session
 from schemas import TaskCreate, CategoryCreate, TaskGet, CategoryGet, TokenGet, UserCreate, UserGet
 from repositories import TaskRepository, CategoryRepository, UserRepository
-from dependencies import SessionDep, LoginFormDep
+from dependencies import SessionDep, LoginFormDep, UserDep
 from security import verify_password_hash, DUMMY_HASH, create_access_token
 
 router = APIRouter()
 
-@router.post("/login", response_model=TokenGet)
+@router.post("/user", summary="Создание пользователя", response_model=UserGet)
+async def create_user(user: UserCreate, session: SessionDep):
+    repo = UserRepository(session)
+    return await repo.create(user)
+
+@router.post("/login", summary="Аутентификация пользователя", response_model=TokenGet)
 async def get_token(async_session: SessionDep, form: LoginFormDep):
     invalid_credentials_exp = HTTPException(
             status_code=401,
@@ -27,23 +30,18 @@ async def get_token(async_session: SessionDep, form: LoginFormDep):
     }
     return response
 
-@router.post("/task")
-async def create_task(task: TaskCreate, session: AsyncSession = Depends(get_async_session)):
+@router.post("/task", summary="Создание задачи", response_model=TaskGet)
+async def create_task(task: TaskCreate, session: SessionDep, user: UserDep):
     repo = TaskRepository(session)
     return await repo.create(task)
 
-@router.post("/category")
-async def create_category(category: CategoryCreate, session: AsyncSession = Depends(get_async_session)):
+@router.post("/category", summary="Создание категории", response_model=CategoryGet)
+async def create_category(category: CategoryCreate, session: SessionDep, user: UserDep):
     repo = CategoryRepository(session)
     return await repo.create(category)
 
-@router.get("/task", summary="Получить задачи", response_model=list[TaskGet])
-async def get_tasks(limit: int, offset: int, session: AsyncSession = Depends(get_async_session)):
+@router.get("/task", summary="Получение задач", response_model=list[TaskGet])
+async def get_tasks(limit: int, offset: int, session: SessionDep, user: UserDep):
     repo = TaskRepository(session)
     return await repo.get_all(limit, offset)
-
-@router.post("/user", summary="Создание пользователя", response_model=UserGet)
-async def create_user(user: UserCreate, session: AsyncSession = Depends(get_async_session)):
-    repo = UserRepository(session)
-    return await repo.create(user)
 
