12b5f8d603b4165a90cd9f88f4f1c7975640df1a
kiteofyte <keruells@gmail.com>
2026-04-14 17:17:12 +0300

    feature(security): Создана проверки jwt

--- backend/app/security.py
+++ backend/app/security.py
@@ -1,8 +1,9 @@
 from pwdlib import PasswordHash
 from datetime import datetime, timedelta
 import jwt
+from jwt.exceptions import InvalidTokenError
 from config import JWT_SECRET_KEY, JWT_ALGORITHM, JWT_EXPIRATION_HOURS
-
+from fastapi import HTTPException
 
 password_hash = PasswordHash.recommended()
 
@@ -20,3 +21,18 @@ def create_access_token(user_id: int) -> str:
         "exp": datetime.now() + timedelta(hours=JWT_EXPIRATION_HOURS)
     }
     return jwt.encode(payload=payload, key=JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
+
+def verify_token(token: str) -> int:
+    credentials_exception = HTTPException(
+            status_code=401, 
+            detail="Could not validate credentials", 
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    try:
+        payload = jwt.decode(payload=token, key=JWT_SECRET_KEY, algorithms=[JWT_ALGORITHM])
+        user_id = payload.get("sub")
+        if user_id is None:
+            raise credentials_exception
+        return int(user_id)
+    except InvalidTokenError:
+        raise credentials_exception
