41fd6a994277c0ff95d7d42375951781004b8abf
kiteofyte <keruells@gmail.com>
2026-04-26 15:59:49 +0300

    feature(database): Реализован новый репозиторий для управления refresh-токенами

--- backend/app/repositories.py
+++ backend/app/repositories.py
@@ -1,9 +1,9 @@
 from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
+from sqlalchemy import select, delete
 from security import get_password_hash
-from models import Task, Category, User
-from schemas import TaskCreate, CategoryCreate, UserCreate
+from models import Task, Category, User, RefreshToken
+from schemas import TaskCreate, CategoryCreate, UserCreate, RefreshTokenCreate
 from sqlalchemy.orm import selectinload
 
 class TaskRepository:
@@ -73,4 +73,24 @@ class UserRepository():
         result = await self.session.execute(query)
         return result.scalar_one_or_none()
 
-        
+class RefreshTokenRepository():
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def create(self, token: RefreshTokenCreate):
+        token = token.model_dump()
+        db_token = RefreshToken(**token)
+        self.session.add(db_token)
+        await self.session.commit()
+        await self.session.refresh(db_token)
+        return db_token
+
+    async def get(self, token: str):
+        query = select(RefreshToken).where(RefreshToken.token == token)
+        result = await self.session.execute(query)
+        return result.scalar_one_or_none()
+    
+    async def delete(self, token: str):
+        query = delete(RefreshToken).where(RefreshToken.token == token)
+        await self.session.execute(query)
+        await self.session.commit()
