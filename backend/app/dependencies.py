7dc2b93a4b58f0a65187364a294d8328adf7226b
kiteofyte <keruells@gmail.com>
2026-04-26 15:58:55 +0300

    refactor(auth): Удалено обращение к базе для поиска пользователя

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -12,17 +12,7 @@ SessionDep = Annotated[AsyncSession, Depends(get_async_session)]
 LoginFormDep = Annotated[OAuth2PasswordRequestForm, Depends(OAuth2PasswordRequestForm)]
 TokenDep = Annotated[str, Depends(oauth2_scheme)]
 
-async def get_current_user(async_session: SessionDep, token: TokenDep):
-    credentials_exception = HTTPException(
-        status_code=401, 
-        detail="Could not validate credentials", 
-        headers={"WWW-Authenticate": "Bearer"}
-    )
-    user_id = verify_token(token)
-    repo = UserRepository(session=async_session)
-    user = await repo.get_by_id(user_id=user_id)
-    if user is None:
-        raise credentials_exception
-    return user
+async def get_current_user_id(token: TokenDep):
+    return verify_token(token)
 
-UserDep = Annotated[object, Depends(get_current_user)]
+UserDep = Annotated[int, Depends(get_current_user_id)]
