d5bf6bdba3717c95d293edd066ac976c24fbc2e3
kiteofyte <keruells@gmail.com>
2026-05-06 20:49:43 +0300

    fetature(dep): Для получения фильтров из query параметров добавлена соответствующая зависимость , которая будет использоваться в /task

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -5,12 +5,14 @@ from database import get_async_session
 from fastapi import Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from schemas import TaskFilter
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
 
 SessionDep = Annotated[AsyncSession, Depends(get_async_session)]
 LoginFormDep = Annotated[OAuth2PasswordRequestForm, Depends(OAuth2PasswordRequestForm)]
 TokenDep = Annotated[str, Depends(oauth2_scheme)]
+TaskFilterDep = Annotated[TaskFilter, Depends()]
 
 async def get_current_user_id(token: TokenDep):
     return verify_token(token)
