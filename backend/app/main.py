f4d73f0f5f3ec8275e8a512bb5212153777e291b
kiteofyte <keruells@gmail.com>
2026-05-06 22:05:21 +0300

    feature(app): Добавлен CORS middleware для работы с фронтендом

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI, HTTPException
 from database import create_database
 from routers import router
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -9,5 +10,13 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(router, prefix="/api/v1")
