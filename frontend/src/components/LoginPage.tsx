93070c12628bed3745c1e55cec4b7f21beeaf0b2
kiteofyte <keruells@gmail.com>
2026-05-13 12:22:02 +0300

    feature(frontend): Написана функция обработчик для кнопки "Войти".

--- frontend/src/components/LoginPage.tsx
+++ frontend/src/components/LoginPage.tsx
@@ -1,8 +1,38 @@
 import { useState } from "react";
 import { LogIn } from "lucide-react";
+import { useAuth } from "./AuthContext";
+import { Navigate, useNavigate } from "react-router-dom";
 
 function LoginPage() {
   const [mode, setMode] = useState("login");
+  const [inputs, setInputs] = useState({ login: "", password: "" });
+  const { token, setToken } = useAuth();
+  const navigate = useNavigate();
+  if (token) {
+    return <Navigate to="/" />;
+  }
+
+  const handleSubmit = async () => {
+    const formData = new URLSearchParams();
+    formData.append("username", inputs.login);
+    formData.append("password", inputs.password);
+
+    const response = await fetch("http://127.0.0.1:8000/api/v1/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: formData,
+    });
+    if (!response.ok) {
+      console.log("Ошибка");
+      return;
+    }
+    const jwt = await response.json();
+    setToken(jwt.access_token);
+    navigate("/");
+  };
+
   return (
     <div className="h-screen flex flex-col items-center justify-center gap-y-5">
       <h1 className="text-2xl text-gray-700 font-bold text-center">Добро пожаловать!</h1>
@@ -26,6 +56,8 @@ function LoginPage() {
         <div className="flex flex-col gap-y-2 text-base font-semibold">
           <label htmlFor="username">Логин</label>
           <input
+            value={inputs.login}
+            onChange={(e) => setInputs({ ...inputs, login: e.target.value })}
             className="py-2 px-4 rounded-md border border-gray-200 shadow-sm focus:outline-none min-w-80"
             type="text"
             id="username"
@@ -37,6 +69,8 @@ function LoginPage() {
         <div className="flex flex-col gap-y-2 text-base font-semibold">
           <label htmlFor="password">Пароль</label>
           <input
+            value={inputs.password}
+            onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
             className="py-2 px-4 rounded-md border border-gray-200 shadow-sm focus:outline-none min-w-80"
             type="password"
             id="password"
@@ -46,6 +80,7 @@ function LoginPage() {
           />
         </div>
         <button
+          onClick={handleSubmit}
           className={`cursor-pointer p-2 shadow-sm text-base font-semibold
            hover:bg-amber-200 transition-all duration-500 rounded-sm w-fit flex items-center gap-2 m-auto`}
         >
