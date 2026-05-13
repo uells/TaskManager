a159ea71a6e011a88def0a8392a0afadf7227482
kiteofyte <keruells@gmail.com>
2026-05-13 12:50:17 +0300

    feature(frontend): Написан ProtectedRoute - компонет который проверяет аутентифицирован ли пользователь, и если нет -- перенаправляет его на страницу входа

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
 import TaskPage from "./components/TaskPage";
 import LoginPage from "./components/LoginPage";
 import { Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />}></Route>
-      <Route path="/" element={<TaskPage />}></Route>
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <TaskPage />
+          </ProtectedRoute>
+        }
+      ></Route>
     </Routes>
   );
 }
---
+++ frontend/src/components/ProtectedRoute.tsx
@@ -0,0 +1,17 @@
+import { type ReactNode } from "react";
+import { useAuth } from "./AuthContext";
+import { Navigate } from "react-router-dom";
+
+type Props = {
+  children: ReactNode;
+};
+
+function ProtectedRoute({ children }: Props) {
+  const { token } = useAuth();
+  if (!token) {
+    return <Navigate to={"/login"} />;
+  }
+  return <>{children}</>;
+}
+
+export default ProtectedRoute;
